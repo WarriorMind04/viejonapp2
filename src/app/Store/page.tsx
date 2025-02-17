5e3c78fd461743e6081288c81241eb62c5636dd4
WarriorMind04 <josemiguel.guerrerojim@gmail.com>
2025-02-17 12:46:16 -0600

    Creación de Componente MEnú y Paghina Storeú

--- src/app/Store/page.tsx
+++ src/app/Store/page.tsx
@@ -1,3 +1,5 @@
+import Menu from "../Components/menu";
+
 const products = [
   {
     id: 1,
@@ -15,6 +17,7 @@ const products = [
 export default function Store() {
   return (
     <div className="bg-white">
+      <Menu />
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Customers also purchased
